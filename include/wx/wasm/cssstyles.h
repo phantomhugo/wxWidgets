ac9bc4ea3bf466cbab2f56e360cd0f88d446b87d
Hugo Armando Castellanos Morales <phantomhugo@gmail.com>
2026-05-30 15:12:25 -0500

    Add css styles

---
+++ include/wx/wasm/cssstyles.h
@@ -0,0 +1,41 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        wx/wasm/cssstyles.h
+// Purpose:     CSS Style management for WASM port
+// Author:      wxWidgets dev team
+// Created:     2024
+// Copyright:   (c) 2024 wxWidgets dev team
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _WX_WASM_CSSSTYLES_H_
+#define _WX_WASM_CSSSTYLES_H_
+
+#include "wx/string.h"
+#include "wx/defs.h"
+
+// Clase para gestionar estilos CSS
+class WXDLLIMPEXP_CORE wxWasmCSSManager
+{
+public:
+    // Inyecta CSS por defecto si no existe CSS personalizado
+    static void InjectDefaultStyles();
+    
+    // Permite al usuario cargar CSS personalizado
+    static void LoadCustomCSS(const wxString& cssContent);
+    
+    // Obtiene el namespace CSS para una clase wxWidgets
+    static wxString GetCSSClass(const wxString& wxClassName);
+    
+    // Verifica si existe CSS personalizado
+    static bool HasCustomCSS();
+    
+private:
+    static bool ms_defaultStylesInjected;
+    static bool ms_hasCustomCSS;
+};
+
+// Macro helper para aplicar clases CSS a elementos
+#define WX_WASM_CSS_CLASS(className) \
+    wxWasmCSSManager::GetCSSClass(wxT(#className))
+
+#endif // _WX_WASM_CSSSTYLES_H_
